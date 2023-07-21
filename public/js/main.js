9aeca16362ac372f440501d20fa7b9f8f7504366
youssefsadouk <contact.sadouk@gmail.com>
2023-07-21 12:36:14 +0100

    registering event listenners for personalKey copy button functionality

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,10 @@ import * as wss from "./websockets.js"
 const socket = io("/");
 
 wss.registerSocketEvents(socket);
+
+const personalkeycopyBtn = document.getElementById("personal_key_copy_button");
+
+personalkeycopyBtn.addEventListener("click", ()=>{
+    const personal_key= store.getState().socketId;
+    navigator.clipboard.writeText(personal_key);
+})
